a643cffd2a096559b4400e9f3ad3f541d571ffe0
Vanessa-D <vanessa.ducloux@laposte.net>
2021-04-28 11:39:53 +0200

    first

---
+++ script.js
@@ -0,0 +1,13 @@
+// Modification du DOM --> Supprimer des éléments
+
+// $ --> JQuery() permet de transformer l'élément en objet JQuery
+
+// La méthode remove() supprime l'élément ciblé par la classe
+
+$(document).ready(function(){
+    $('.to_remove').remove();
+});
+
+
+
+// En JS vanilla, on aurait :
